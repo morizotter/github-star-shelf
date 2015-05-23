02303c1ed1d00bbe5f666b24e8aa54107ddc4fa7
Naoki Morita <namorit@gmail.com>
2015-05-24 01:04:37 +0900

    Setup devise

--- app/controllers/sample_controller.rb
+++ app/controllers/sample_controller.rb
@@ -1,9 +1,12 @@
 class SampleController < ApplicationController
 
+  before_action :authenticate_user!
+
   def index
   end
 
   def action_1
+    binding.pry
     redirect_to 'index'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   root 'sample#index'
 
   resources 'sample', only: [:index] do
